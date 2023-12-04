398fb9a84ba094dfefbdd99b4748d4e7957ff319
TsikyAro <119358772+TsikyAro@users.noreply.github.com>
2023-12-04 09:55:43 +0300

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,6 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Statistique from './pages/Statistique';
-
 setupIonicReact();
 
 const App: React.FC = () => (
